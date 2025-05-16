dbd7fac847ed508585640d1b72f2323d5e829933
Xiaoyu Luo <xiaoyuluo@h205.aau1x.klient.sydhavn2.site.aau.dk>
2025-05-16 14:04:26 +0200

    update test usecase

--- example/cudnn_test/cudnn_utils.h
+++ example/cudnn_test/cudnn_utils.h
@@ -1,36 +1,31 @@
-#ifndef CUDNN_UTILS_H
-#define CUDNN_UTILS_H
-
-#include <iostream>
-#include <cudnn.h>
-#include <cuda_runtime.h>
-
-#define checkCUDNN(expression)                                          \
-    {                                                                   \
-        cudnnStatus_t status = (expression);                            \
-        if (status != CUDNN_STATUS_SUCCESS) {                           \
-            std::cerr << "cuDNN Error on line " << __LINE__ << ": "     \
-                      << cudnnGetErrorString(status)                    \
-                      << " at " << #expression << std::endl;            \
-            std::exit(EXIT_FAILURE);                                     \
-        } else {                                                        \
-            std::cout << "cuDNN call succeeded: " << #expression        \
-                      << " at line " << __LINE__ << std::endl;          \
-        }                                                               \
+#define checkCUDNN(expression)                                              \
+    {                                                                       \
+        cudnnStatus_t status = (expression);                                \
+        if (status != CUDNN_STATUS_SUCCESS) {                               \
+            std::cerr << "cuDNN Error in file " << __FILE__                 \
+                      << " at line " << __LINE__ << ": "                    \
+                      << cudnnGetErrorString(status)                        \
+                      << " during " << #expression << std::endl;            \
+            std::exit(EXIT_FAILURE);                                        \
+        } else {                                                            \
+            std::cout << "cuDNN call succeeded: " << #expression            \
+                      << " in file " << __FILE__                            \
+                      << " at line " << __LINE__ << std::endl;              \
+        }                                                                   \
     }
 
-#define checkCUDA(expression)                                           \
-    {                                                                   \
-        cudaError_t status = (expression);                              \
-        if (status != cudaSuccess) {                                    \
-            std::cerr << "CUDA Error on line " << __LINE__ << ": "      \
-                      << cudaGetErrorString(status)                     \
-                      << " at " << #expression << std::endl;            \
-            std::exit(EXIT_FAILURE);                                     \
-        } else {                                                        \
-            std::cout << "CUDA call succeeded: " << #expression         \
-                      << " at line " << __LINE__ << std::endl;          \
-        }                                                               \
+#define checkCUDA(expression)                                               \
+    {                                                                       \
+        cudaError_t status = (expression);                                  \
+        if (status != cudaSuccess) {                                        \
+            std::cerr << "CUDA Error in file " << __FILE__                  \
+                      << " at line " << __LINE__ << ": "                    \
+                      << cudaGetErrorString(status)                         \
+                      << " during " << #expression << std::endl;            \
+            std::exit(EXIT_FAILURE);                                        \
+        } else {                                                            \
+            std::cout << "CUDA call succeeded: " << #expression             \
+                      << " in file " << __FILE__                            \
+                      << " at line " << __LINE__ << std::endl;              \
+        }                                                                   \
     }
-
-#endif // CUDNN_UTILS_H
